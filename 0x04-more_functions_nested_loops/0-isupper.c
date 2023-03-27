305c6f805fac71667a77975c450bcabc9a373132
saifelden1 <123162932+saifelden1@users.noreply.github.com>
2023-03-27 04:21:29 +0200

    Add files via upload

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,31 @@
+/*
+*
+* File: 0-isupper.c
+*
+* Auth: Omar_KAhmed
+*
+*/
+
+
+#include "main.h"
+
+/**
+* _isupper - checks for uppercase character
+* @c: Variable text
+* Return: Always 0.
+*/
+
+int _isupper(int c)
+{
+
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
